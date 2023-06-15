2b8c9c822d6f224950392ccf980be7f5e7ea13f0
TheRookieStudent <90093702+TheRookieStudent@users.noreply.github.com>
2023-06-15 20:48:42 +0200

    Update axios-auth.js

--- flask_app_frontend/src/axios-auth.js
+++ flask_app_frontend/src/axios-auth.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-//this code says where to create a connection with and on which port. Axios is used to make requests
+//this code says where to create a connection with and on which port. Axios is used to make requests. The back-end is with swagger Ui of port 5000
 const instance = axios.create({
     baseURL: 'http://localhost:5000/'
 });
